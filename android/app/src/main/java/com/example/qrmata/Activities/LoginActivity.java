2e2bf02b65c06d782ea5e6a1881b79ccee2c88be
EstebanMG <esteban.mata.gz20@gmail.com>
2019-11-27 13:55:08 -0600

    Agregando imagenes

--- android/app/src/main/java/com/example/qrmata/Activities/LoginActivity.java
+++ android/app/src/main/java/com/example/qrmata/Activities/LoginActivity.java
@@ -2,13 +2,51 @@ package com.example.qrmata.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.qrmata.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener  {
+
+
+    private TextInputEditText _txtEditUsername,_txtEditPassword;
+    private TextInputLayout _txtLayoutUsername,_txtLayoutPassword;
+    private MaterialButton _btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void initializeComponents(){
+        _txtLayoutUsername = findViewById(R.id.txtUser);
+        _txtLayoutPassword = findViewById(R.id.txtPassword);
+        _btnLogin = findViewById(R.id.btnLogin);
+    }
+
+    public void getCredentials(){
+        String userEmail = _txtLayoutUsername.getEditText().getText().toString();
+        String userPassword = _txtLayoutPassword.getEditText().getText().toString();
+    }
+
+    public void validateCredentials(String userEmail, String userPassword){
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnLogin:
+                getCredentials();
+                Intent intent = new Intent(getApplicationContext(),ScanActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
