d26c46b183632b08896a2092e725cceebb411dde
Alvaro Silva <alvavic67@hotmail.com>
2019-11-27 12:44:44 -0600

    Nueva estructura

---
+++ web/frontend/build/precache-manifest.43abccb4b2d8d9872c673e2447cb65d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceab8df6b911dde289fb6ead0ec612a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3da12cd1b736005541d8",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "732975e084db5a27f6e8",
+    "url": "/static/js/2.bdb9748c.chunk.js"
+  },
+  {
+    "revision": "3da12cd1b736005541d8",
+    "url": "/static/js/main.6b0330ff.chunk.js"
+  },
+  {
+    "revision": "261d7d4c7c831d2f11ee",
+    "url": "/static/js/runtime-main.19709a7d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
