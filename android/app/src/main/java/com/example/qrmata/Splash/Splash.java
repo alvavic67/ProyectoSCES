29144bb95497b40ce3b4803de91dd8235d947908
EstebanMG <esteban.mata.gz20@gmail.com>
2019-11-27 13:14:00 -0600

    Funcionalidad a clase Splash

--- android/app/src/main/java/com/example/qrmata/Splash/Splash.java
+++ android/app/src/main/java/com/example/qrmata/Splash/Splash.java
@@ -1,4 +1,27 @@
 package com.example.qrmata.Splash;
 
-public class Splash {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.qrmata.R;
+
+public class Splash extends AppCompatActivity {
+    private final int DURACION = 3000;
+    //private View img = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(Splash.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION);
+    }
 }
